9b6d273bae14fa010bf0c31eaf148081efb0f551
Maxim Sudakov <grax922@gmail.com>
2021-11-10 17:15:18 +0300

    refactor: console.log

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -6,7 +6,6 @@ const notePath = path.join(__dirname, notes);
 
 
 const readableStream = fs.createReadStream(notePath);
-console.log(readableStream)
 readableStream.on('readable', () => {
   console.log(`${readableStream.read()}`);
   
